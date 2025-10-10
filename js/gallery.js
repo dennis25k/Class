ae76eeef9635795be917a3011d515a3bbad6274a
Dennis25k <den24k@outlook.com>
2025-10-10 12:47:28 -0400

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,15 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+   document.getElementById("image").innerHTML = previewPic.alt;
+   document.getElementById("image").style.backgroundImage = "url('"+ previewPic.src +"')";
+   console.log(previewPic.alt);
+   console.log(previewPic.src)
+   
+	}
+
+	function unDo(){
+   document.getElementById("image").innerHTML = "Hover over an image below to display here.";
+   document.getElementById("image").style.backgroundImage = "url('')";
+		
+	}
